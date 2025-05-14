f7b7ef32ef722f805ce37d869f7e195dfcdd1162
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-05-14 12:01:03 +0300

    remove unused deps & add new architecture with upstash

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # modal serve .\main.py
 from typing import Annotated
 from fastapi import FastAPI, Header, status
-from starlette.responses import JSONResponse
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import modal
+import os
 
 image = (
     modal.Image.debian_slim(python_version="3.12")
@@ -14,8 +15,6 @@ image = (
         "modal>=0.73.91",
         "fastapi[standard]==0.115.11",
         "pydantic==2.9.2",
-        "python-multipart>=0.0.20",
-        "starlette==0.41.2",
         "requests>=2.31.0",
         "redis>=5.2.1",
     )
@@ -31,9 +30,21 @@ image = (
 )
 
 app = modal.App(
-    name="agentset-ingest",
+    name="agentset-ingest-v2",
     image=image,
-    secrets=[modal.Secret.from_name("partitioner-secrets")],
+    secrets=[
+        modal.Secret.from_name(
+            "partitioner-secrets",
+            required_keys=[
+                "QSTASH_URL",
+                "QSTASH_TOKEN",
+                "REDIS_HOST",
+                "REDIS_PORT",
+                "REDIS_PASSWORD",
+                "AGENTSET_API_KEY",
+            ],
+        )
+    ],
 )
 
 web_app = FastAPI()
@@ -46,18 +57,40 @@ class IngestRequest(BaseModel):
     extra_metadata: dict | None = None
     unstructured_args: dict | None = None
     batch_size: int | None = None
-    # tokenizer_model: str | None = None
+    notify_id: str | None = None
 
 
-@web_app.post("/ingest")
-async def ingest(
-    request: IngestRequest,
-    api_key: Annotated[str | None, Header(alias="api-key")] = None,
-):
-    import os
+@app.function()
+@modal.asgi_app()
+def partition_api():
+    return web_app
+
+
+@app.function(timeout=7200)  # 2 hours
+def ingest_operation(request: IngestRequest):
+
+    def notify_workflow(finalStatus, finalBody):
+        body = {"status": finalStatus}
+        body.update(finalBody)
+
+        res = JSONResponse(
+            status_code=finalStatus,
+            content=body,
+        )
+
+        if request.notify_id is None:
+            return res
+
+        requests.post(
+            f"{os.getenv('QSTASH_URL')}/v2/notify/{request.notify_id}",
+            headers={
+                "Authorization": f'Bearer {os.getenv("QSTASH_TOKEN")}',
+            },
+            json=body,
+        )
+        return res
 
     metadata = request.extra_metadata
-
     unstructured_args = {
         "strategy": "auto",  # fast, hi_res, ocr_only, auto
         "chunking_strategy": "basic",  # by_title, basic
@@ -65,32 +98,21 @@ async def ingest(
     if request.unstructured_args:
         unstructured_args.update(request.unstructured_args)
 
-    if api_key != os.getenv("AGENTSET_API_KEY"):
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={
-                "status": status.HTTP_401_UNAUTHORIZED,
-                "message": "api-key is not valid!",
-            },
-        )
-
     # Count how many input sources are provided
     input_sources = sum(1 for x in [request.url, request.text] if x is not None)
 
     if input_sources == 0:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content={
-                "status": status.HTTP_400_BAD_REQUEST,
+        return notify_workflow(
+            status.HTTP_400_BAD_REQUEST,
+            {
                 "message": "Either url or text must be provided",
             },
         )
 
     if input_sources > 1:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content={
-                "status": status.HTTP_400_BAD_REQUEST,
+        return notify_workflow(
+            status.HTTP_400_BAD_REQUEST,
+            {
                 "message": "Only one of url or text can be provided",
             },
         )
@@ -98,7 +120,6 @@ async def ingest(
     from io import BytesIO
     from llama_index.readers.file import UnstructuredReader
 
-    # import tiktoken
     from unstructured.file_utils.filetype import detect_filetype
     import requests
     from redis import Redis
@@ -117,10 +138,9 @@ async def ingest(
                 size_in_bytes = len(response.content)
                 file_stream = BytesIO(response.content)
             except Exception as e:
-                return JSONResponse(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    content={
-                        "status": status.HTTP_400_BAD_REQUEST,
+                return notify_workflow(
+                    status.HTTP_400_BAD_REQUEST,
+                    {
                         "message": f"Failed to download file from URL: {str(e)}",
                     },
                 )
@@ -147,10 +167,9 @@ async def ingest(
         )
 
         if len(documents) <= 0:
-            return JSONResponse(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                content={
-                    "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+            return notify_workflow(
+                status.HTTP_500_INTERNAL_SERVER_ERROR,
+                {
                     "message": "couldn't parse document",
                 },
             )
@@ -162,20 +181,13 @@ async def ingest(
 
         final_batch_size = request.batch_size or 5
         total_batches = 0
-        # Initialize tokenizer from LlamaIndex
-        # total_tokens = 0
-        # tokenizer = tiktoken.encoding_for_model(
-        #     request.tokenizer_model
-        #     if request.tokenizer_model
-        #     else "text-embedding-3-large"
-        # )
 
         for document in documents:
             total_chunks += 1
             if document.text:
                 text = document.text
                 total_characters += len(text)
-                # total_tokens += len(tokenizer.encode(text))
+
             if document.metadata.get("page_number"):
                 total_pages = max(
                     total_pages if total_pages is not None else 0,
@@ -193,10 +205,9 @@ async def ingest(
                 # Add to current batch
                 result_documents[-1].append(document.to_dict())
 
-        results_id = uuid.uuid4()
+        results_id = str(uuid.uuid4())
         batch_template = f"results_{results_id}_[BATCH_INDEX]"
         result = {
-            "status": status.HTTP_200_OK,
             "metadata": {
                 "filename": filename_to_use,
                 "filetype": content_type,
@@ -207,7 +218,6 @@ async def ingest(
             "total_batches": total_batches,
             "results_id": results_id,
             "batch_template": batch_template,
-            # "total_tokens": total_tokens,
         }
 
         if total_pages is not None:
@@ -225,21 +235,54 @@ async def ingest(
             redis_key = batch_template.replace("[BATCH_INDEX]", str(batch_idx))
             redis_client.set(redis_key, json.dumps(batch))
 
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content=result,
+        return notify_workflow(
+            status.HTTP_200_OK,
+            result,
         )
     except Exception as e:
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content={
-                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+        return notify_workflow(
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            {
                 "message": str(e),
             },
         )
 
 
-@app.function(timeout=7200)  # 2 hours
-@modal.asgi_app()
-def partition_api():
-    return web_app
+@web_app.post("/ingest")
+async def ingest(
+    request: IngestRequest,
+    api_key: Annotated[str | None, Header(alias="api-key")] = None,
+):
+    if api_key != os.getenv("AGENTSET_API_KEY"):
+        return JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content={
+                "status": status.HTTP_401_UNAUTHORIZED,
+                "message": "api-key is not valid!",
+            },
+        )
+
+    call = ingest_operation.spawn(request)
+    return {"call_id": call.object_id}
+
+
+@web_app.get("/ingest/results/{call_id}")
+async def poll_results(
+    call_id: str,
+    api_key: Annotated[str | None, Header(alias="api-key")] = None,
+):
+    if api_key != os.getenv("AGENTSET_API_KEY"):
+        return JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content={
+                "status": status.HTTP_401_UNAUTHORIZED,
+                "message": "api-key is not valid!",
+            },
+        )
+
+    function_call = modal.FunctionCall.from_id(call_id)
+    try:
+        return function_call.get(timeout=0)
+    except TimeoutError:
+        http_accepted_code = 202
+        return JSONResponse({}, status_code=http_accepted_code)
